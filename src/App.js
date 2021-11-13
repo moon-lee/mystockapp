e0484503573254e842cb56dd02aeed82bacbe1be
moon-lee <lee.moonkyu@gmail.com>
2021-11-14 00:52:45 +1000

    add table

--- src/App.js
+++ src/App.js
@@ -7,28 +7,40 @@ const url = 'asx-research/1.0/companies/ax1/header'
 function App() {
   const [companies, setCompanies] = useState([])
 
-   useEffect(function () {
+  useEffect(function () {
     axios.get(url)
-    .then(response => {
-      console.log(response.data.data)
-      setCompanies(response.data.data)
-    })
+      .then(response => {
+        console.log(response.data.data)
+        setCompanies(response.data.data)
+      })
   }, [])
 
 
   return (
     <div className="App">
       <h1>Stock Fundermental</h1>
-      <ul>
-        {
-          Object.entries(companies).map(([key,value]) => {
-            return (
-              <li>{key} : {value.toString()} </li>
-            )
-          })
-        }
-      </ul>
+      <table>
+        <thead>
+          {
+            Object.entries(companies).map(([key, value]) => {
+              return (
+                <th>{key}</th>
+              )
+            })
+          }
 
+        </thead>
+        <tbody>
+          {
+            Object.entries(companies).map(([key, value]) => {
+              return (
+                <td>{value.toString()}</td>
+              )
+            })
+          }
+
+        </tbody>
+      </table>
     </div>
   );
 }
