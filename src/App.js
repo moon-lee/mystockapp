1baf45de98bdc5b5fee4b1914348911a4c9a5299
moon-lee <lee.moonkyu@gmail.com>
2021-11-20 10:30:29 +1000

    little changes

--- src/App.js
+++ src/App.js
@@ -6,72 +6,72 @@ const company_info = '/asx-research/1.0/companies/ax1/header'
 const asx_list = '/asx-research/1.0/companies/ax1/key-statistics'
 
 var options = {
-  method: 'GET',
-  url: 'https://yfapi.net/v11/finance/quoteSummary/AX1.AX',
-  params: {
-    lang:'en',
-    region:'AU',
-    modules: 'defaultkeyStatistics',
-  },
-  headers: {
-    'x-api-key': 'qoFQOEOBAm7gUeuBfZEqq7Tq0ylgxP228nZsL79D'
-  }
+	method: 'GET',
+	url: 'https://yfapi.net/v11/finance/quoteSummary/AX1.AX',
+	params: {
+		lang: 'en',
+		region: 'AU',
+		modules: 'defaultkeyStatistics',
+	},
+	headers: {
+		'x-api-key': 'qoFQOEOBAm7gUeuBfZEqq7Tq0ylgxP228nZsL79D'
+	}
 };
 
 function App() {
-  const [companies, setCompanies] = useState([])
-  //const [asx_companies, setAsxCompanies] = useState([])
+	const [companies, setCompanies] = useState([])
+	//const [asx_companies, setAsxCompanies] = useState([])
 
-  useEffect(function () {
-    axios.get(company_info)
-      .then(response => {
-        //console.log(response.data.data)
-        setCompanies(response.data.data)
-      })
-  }, [])
-
-
-  useEffect(function () {
-    axios.get(asx_list)
-      .then(response => {
-        console.log(response.data)
-        //setAsxCompanies(response.data)
-      })
-  }, [])
+	useEffect(function () {
+		axios.get(company_info)
+			.then(response => {
+				//console.log(response.data.data)
+				setCompanies(response.data.data)
+			})
+	}, [])
 
 
-  useEffect(function () {
-    axios.request(options)
-      .then(response => {
-        console.log(response.data)
-        //setAsxCompanies(response.data)
-      })
-  }, [])
+	useEffect(function () {
+		axios.get(asx_list)
+			.then(response => {
+				console.log(response.data)
+				//setAsxCompanies(response.data)
+			})
+	}, [])
 
 
+	useEffect(function () {
+		axios.request(options)
+			.then(response => {
+				console.log(response.data)
+				//setAsxCompanies(response.data)
+			})
+	}, [])
 
 
 
-  return (
-    <div className="App">
-      <h1>Stock Fundermental</h1>
-      <table>
 
-        {
-          Object.entries(companies).map(([key, value]) => {
-            return (
-              <tr>
-                <th>{key}</th>
-                <td>{value.toString()}</td>
-              </tr>
-            )
-          })
-        }
 
-      </table>
+	return (
+		<div className="App">
+			<h1>Stock Fundermental</h1>
+			<table>
 
-    </div>
-  );
+				{
+					Object.entries(companies).map(([key, value]) => {
+						return (
+							<tr>
+								<th>{key}</th>
+								<td>{value.toString()}</td>
+							</tr>
+						)
+					})
+				}
+
+			</table>
+
+		</div>
+	);
 }
 
 export default App;
